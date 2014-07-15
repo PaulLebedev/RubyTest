74bb134275d2f8eefb776524696b76b2f6ca69ee
Paul Lebedev <PaulLebedev@mail.ru>
2014-07-15 15:09:54 +0400

    Make a basic User model (including secure passwords)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,7 +10,10 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
-ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
+#If you're running Rails 4.1:
+ActiveRecord::Migration.maintain_test_schema!
+#If you're running Rails 4:
+#ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
 RSpec.configure do |config|
   # ## Mock Framework
